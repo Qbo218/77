4713bc3247c9dfac5eb986c3af23fbfd41cd3e33
Qbo218 <feb.qbo@gmail.com>
2021-03-19 15:21:06 +0800

    Update Bili_Auto_Regions.js

--- Surge/Script/JS/Bili_Auto_Regions.js
+++ Surge/Script/JS/Bili_Auto_Regions.js
@@ -8,23 +8,24 @@ Bilibli番剧，自动切换地区
 您需要配置相关规则集（https://raw.githubusercontent.com/DivineEngine/Profiles/master/Surge/Ruleset/StreamingMedia/StreamingSE.list）绑定相关select策略组，并且需要具有相关的区域代理服务器纳入您的子策略中。
 最后，您可以通过BoxJs设置策略名和子策略名，或者手动填入脚本。
 
+Update: 2021.02.07
 Author: @NobyDa
 
 ****************************
 Surge 4.2+ 远程脚本配置 :
 ****************************
 [Script]
-Bili Region = type=http-response,pattern=https:\/\/api\.bilibili\.com\/pgc\/view\/(v\d\/)?app\/season\?access_key,requires-body=1,max-size=0,control-api=1,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/Bili_Auto_Regions.js
+Bili Region = type=http-response,pattern=^https:\/\/ap(p|i)\.bilibili\.com\/(pgc\/view\/(v\d\/)?app|x(\/v\d)?\/view\/video)\/(season|online)\?access_key,requires-body=1,max-size=0,control-api=1,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/Bili_Auto_Regions.js
 
 [MITM]
-hostname = api.bilibili.com
+hostname = ap?.bilibili.com
 ****************************
 */
 
 const Group = $persistentStore.read('BiliArea_Policy') || '策略：港台媒体'; //Your blibli policy group name.
-const CN = $persistentStore.read('BiliArea_CN') || '直接连接'; //Your China sub-policy name.
-const TW = $persistentStore.read('BiliArea_TW') || '顺序：台新节点'; //Your Taiwan sub-policy name.
-const HK = $persistentStore.read('BiliArea_HK') || '顺序：香港节点'; //Your HongKong sub-policy name.
+const CN = $persistentStore.read('指定：直接连接') || 'DIRECT'; //Your China sub-policy name.
+const TW = $persistentStore.read('可用：台新节点') || '🇹🇼 sub-policy'; //Your Taiwan sub-policy name.
+const HK = $persistentStore.read('可用：香港节点') || '🇭🇰 sub-policy'; //Your HongKong sub-policy name.
 
 var obj = JSON.parse($response.body),
 	obj = (obj.result || obj.data || {}).title || '';
@@ -37,7 +38,7 @@ const str = (() => {
 	} else if (current != CN) return CN;
 })()
 
-if (str && obj) {
+if (str) {
 	const change = $surge.setSelectGroupPolicy(Group, str);
 	const notify = $persistentStore.read('BiliAreaNotify') === 'true';
 	if (!notify) $notification.post(obj, ``, `${current}  =>  ${str}  =>  ${change?`🟢`:`🔴`}`);
